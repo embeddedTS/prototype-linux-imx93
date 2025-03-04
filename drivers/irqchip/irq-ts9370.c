acb7e206936fb699cace98c060a61d79cb17c565
Mark Featherston <mark@embeddedts.com>
2025-03-04 10:46:22 -0700

    drivers: irq-ts9370: Fix license comment

--- drivers/irqchip/irq-ts9370.c
+++ drivers/irqchip/irq-ts9370.c
@@ -1,11 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0
 /*
- * Multiplexed-IRQs driver for TS-9370's FPGA
- *
  * Copyright (c) 2025 - Technologic Systems, DBA embeddedTS
  *
- * This file is licensed under the terms of the GNU General Public
- * License version 2. This program is licensed "as is" without any
- * warranty of any kind, whether express or implied.
  */
 
 #include <linux/interrupt.h>
